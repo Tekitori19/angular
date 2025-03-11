1f4d5de1ee182228036663def5ad9fbf44f3f48c
Dwcks <dinhpdpd09993@fpt.edu.vn>
2025-03-11 10:29:16 +0700

    ad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lab1';
+  title = 'Phan Dương Định';
+  mssv = 'MSSV: PD09993';
 }
